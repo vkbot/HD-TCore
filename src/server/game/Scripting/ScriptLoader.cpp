6af0c3a596e1718ea2bed37eecb633ae35afccad
Karlos <hachasydados@gmail.com>
2011-07-08 20:12:39 +0200

    HD 089. Vuelve el vendedor de lotería de HyD

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1249,6 +1249,7 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 void AddSC_custom_fixes();
+void AddSC_hyd_lottery();
 #endif
 
 void AddCustomScripts()
@@ -1256,5 +1257,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 AddSC_custom_fixes();
+AddSC_hyd_lottery();
 #endif
 }

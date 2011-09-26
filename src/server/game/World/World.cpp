db2b6c00c1fc0f574843878e14641ff45916fab0
Karlos <hachasydados@gmail.com>
2011-09-26 10:10:14 +0200

    HD 006. Passive Anticheat

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1185,6 +1185,10 @@ void World::LoadConfigSettings(bool reload)
     // MySQL ping time interval
     m_int_configs[CONFIG_DB_PING_INTERVAL] = sConfig->GetIntDefault("MaxPingTime", 30);
 
+    m_bool_configs[CONFIG_ANTICHEAT_ENABLE] = sConfig->GetBoolDefault("Anticheat.Enable", true);
+    m_int_configs[CONFIG_ANTICHEAT_REPORTS_INGAME_NOTIFICATION] = sConfig->GetIntDefault("Anticheat.ReportsForIngameWarnings", 70);
+    m_int_configs[CONFIG_ANTICHEAT_DETECTIONS_ENABLED] = sConfig->GetIntDefault("Anticheat.DetectionsEnabled",31);
+
     sScriptMgr->OnConfigLoad(reload);
 }
 

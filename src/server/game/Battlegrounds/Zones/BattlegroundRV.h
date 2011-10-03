29beb5fd9cc609b62922f0ec6c7a2e74fc3585b6
Souler <soulerhyd@gmail.com>
2011-10-04 01:09:11 +0200

    Eliminados numeros mágicos en la altura y radio de los pilares. Ahora existen variables para ellos.

--- src/server/game/Battlegrounds/Zones/BattlegroundRV.h
+++ src/server/game/Battlegrounds/Zones/BattlegroundRV.h
@@ -91,6 +91,11 @@ enum BattlegroundRVData
     BG_RV_WORLD_STATE                            = 0xe1a,
 };
 
+#define BG_RV_PILAR_SMALL_RADIUS  1.25f
+#define BG_RV_PILAR_SMALL_HEIGHT  5.0f
+#define BG_RV_PILAR_BIG_RADIUS    5.0f
+#define BG_RV_PILAR_BIG_HEIGHT    8.25f
+
 class BattlegroundRVScore : public BattlegroundScore
 {
     public:

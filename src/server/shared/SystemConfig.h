14a170b822f8cf4db2942072983f008d82457100
vkbot <info@exalted-world.pp.ua>
2011-10-22 14:09:44 +0300

    Копирайты

--- src/server/shared/SystemConfig.h
+++ src/server/shared/SystemConfig.h
@@ -24,7 +24,7 @@
 #include "Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "Hachas & Dados T-Core "
+#define _PACKAGENAME "PvP-Masters x130 "
 
 #if TRINITY_ENDIAN == TRINITY_BIGENDIAN
 # define _ENDIAN_STRING "big-endian"
@@ -39,6 +39,6 @@
 #  define _FULLVERSION _PACKAGENAME " rev. " VER_PRODUCTVERSION_STR " (Win32, " _BUILD_DIRECTIVE ")"
 # endif
 #else
-#  define _FULLVERSION _PACKAGENAME " rev. " VER_PRODUCTVERSION_STR " (Unix, " _BUILD_DIRECTIVE ")"
+#  define _FULLVERSION _PACKAGENAME " rev. 0.94 (Unix). Last core update: 22.10.2011 "
 #endif
 #endif
